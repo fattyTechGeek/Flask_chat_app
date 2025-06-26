e997f9f56a73947ce514adc97fad5e6cb10b5af1
c30049693 <cuiyuchao2@huawei.com>
2025-06-26 10:17:41 +0800

    bug fix

--- src/main.py
+++ src/main.py
@@ -77,4 +77,4 @@ def handle_message(data):
     }, room=room)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    socketio.run(app, host='0.0.0.0', allow_unsafe_werkzeug=True, port=5000, debug=True)
